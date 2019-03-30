b697c13fb70ab8ddc5e797f11ff281c1504f8691
Rohail Kabani <rohail.kabani@gmail.com>
2019-03-30 12:59:04 -0400

    wip

---
+++ website-v2/src/components/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AboutContainer = styled.div`
+  background-color: #fed837;
+`;
+
+export default () => (
+  <AboutContainer>
+    <p>React stuff dude</p>
+  </AboutContainer>
+)
