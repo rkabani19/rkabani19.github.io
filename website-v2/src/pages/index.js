ebaaf34016d14b6c960ba26fd56d54e1c9863183
Rohail Kabani <rohail.kabani@gmail.com>
2019-03-30 12:59:04 -0400

    wip

--- website-v2/src/pages/index.js
+++ website-v2/src/pages/index.js
@@ -1,21 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import styled from 'styled-components';
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import About from "../components/about"
+
+const LeftContainer = styled.div`
+  display: flex;
+  flex: 1;
+  height: 200vh;
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  flex: 1;
+  height: 200vh;
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+`;
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <MainContainer>
+    <LeftContainer>
+      <About />
+    </LeftContainer>    
+    <RightContainer>
+      <p>Place Holder</p>
+    </RightContainer>
+  </MainContainer>
 )
 
 export default IndexPage
