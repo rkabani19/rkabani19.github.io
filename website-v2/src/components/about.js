040d78b11b877b77bdc1f4d95f138604c7a9be7c
Rohail Kabani <rohail.kabani@gmail.com>
2019-03-30 13:57:05 -0400

    add styling

--- website-v2/src/components/about.js
+++ website-v2/src/components/about.js
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 
 const AboutContainer = styled.div`
   background-color: #fed837;
+  height: 100%;
 `;
 
 export default () => (
   <AboutContainer>
-    <p>React stuff dude</p>
   </AboutContainer>
 )
