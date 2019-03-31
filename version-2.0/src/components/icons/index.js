bd437ca61d63e0d10fbab21ef8830827e3d256cc
Rohail Kabani <rohail.kabani@gmail.com>
2019-03-31 19:31:42 -0400

    reset

---
+++ version-2.0/src/components/icons/index.js
@@ -0,0 +1,11 @@
+import IconGithub from './github';
+import IconLinkedin from './linkedin';
+import IconFacebook from './facebook';
+import IconEmail from './email';
+
+ export {
+  IconGithub,
+  IconLinkedin,
+  IconFacebook,
+  IconEmail,
+};
---
+++ version-2.0/src/styles/theme.js
@@ -0,0 +1,21 @@
+const theme = {
+  colors: {
+    whitish: '#F7F6F5',
+    navy: '#011F44',
+    bluish: '#003A85',
+    brightRed: '#FF3E55',
+  },
+
+  fontSizes: {
+    xsmall: '12px',
+    smallish: '13px',
+    small: '14px',
+    medium: '16px',
+    large: '18px',
+    xlarge: '20px',
+    xxlarge: '22px',
+    h3: '32px',
+  },
+};
+
+export default theme;
